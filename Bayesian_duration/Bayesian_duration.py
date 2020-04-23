782639734a736a64a2add634d0c4e296dd6bcfc5
zoujinhang <653538607@qq.com>
2020-04-23 20:40:20 +0800

    更新

--- Bayesian_duration/Bayesian_duration.py
+++ Bayesian_duration/Bayesian_duration.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from .Baseline import WhittakerSmooth
+from ..Baseline import WhittakerSmooth
 
 def background_correction(t,rate,edges,backgroundsize = 10,degree = 5,plot_save = None):
 	'''
